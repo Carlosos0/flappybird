643f2d87ba8751f2485cae874367306c5857bcf7
Carlos <carloscordeiro130520@gmail.com>
2026-09-02 15:42:47 -0300

    Correção da saída do Doxygen para funcionar com GitHub Pages

---
+++ docs/search/files_9.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['main_2ecpp_0',['main.cpp',['../main_8cpp.html',1,'']]],
+  ['mapmenu_2ecpp_1',['mapMenu.cpp',['../mapMenu_8cpp.html',1,'']]],
+  ['mapmenu_2ehpp_2',['mapMenu.hpp',['../mapMenu_8hpp.html',1,'']]],
+  ['menu_2ehpp_3',['menu.hpp',['../menu_8hpp.html',1,'']]]
+];
--- docs/search/searchdata.js
+++ docs/search/searchdata.js
@@ -1,13 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "abcdefghijlmnopqrstuvwxy~—",
+  0: "abcdefghijlmnopqrstuvwxy~",
   1: "abcdefgilmnpsv",
   2: "abcdefgilmnprsv",
   3: "abcdefghijlmnoprsuvw~",
   4: "abcdefghijlmnpqrstwxy",
   5: "gm",
-  6: "cdeglmnoprs",
-  7: "bdft—"
+  6: "cdeglmnoprs"
 };
 
 var indexSectionNames =
@@ -18,8 +17,7 @@ var indexSectionNames =
   3: "functions",
   4: "variables",
   5: "enums",
-  6: "enumvalues",
-  7: "pages"
+  6: "enumvalues"
 };
 
 var indexSectionLabels =
@@ -30,7 +28,6 @@ var indexSectionLabels =
   3: "Functions",
   4: "Variables",
   5: "Enumerations",
-  6: "Enumerator",
-  7: "Pages"
+  6: "Enumerator"
 };
 
